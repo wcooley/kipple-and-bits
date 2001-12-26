5e52f52a9e829d9013d32ea000b6928203df1400
Wil Cooley <wcooley@nakedape.cc>
2001-12-26 23:59:35 +0000

    create_user: Adds docs and var copies from entry_from_user() and var generation from user_from_form().

--- work/webmin/directory_mgr/ldap_users.pl
+++ work/webmin/directory_mgr/ldap_users.pl
@@ -116,34 +116,108 @@ sub is_mail_free
     return $entry;
 }
 
+=head2 create_user
+
+SYNOPSIS
+
+create_user ( \%user )
+
+DESCRIPTION
+
+Creates a user in the LDAP directory based on attributes in
+user hash.
+
+RETURN VALUE
+
+Returns an array containing, in the case of success, the
+uidNumber and DN of the user (2-element array).
+
+In the case of failure, returns a 2-element array of -1 and
+a formatted error string.
+
+BUGS
+
+OU for users is not configurable.  Attributes for Kerberos and
+posixShadow accounts are not implemented.
+
+=cut
 
 sub create_user
 {
+	my ($user) = @_ ;
     my ($entry, $dn, $err);
 
     $entry = $conn->newEntry ();
-    $dn = "uid=$uid,ou=People," . $config{'base'};
+    $dn = "uid=" . $user->{'uid'} . ",ou=People," . $config{'base'};
     $entry->setDN ($dn);
 
 	# Set add object classes
     $entry->{'objectclass'} = ["posixAccount", "person", "inetOrgPerson",
         "organizationalPerson", "account", "top" ];
-	$config{'kerberos'} and
-		$entry->addValue("objectclass", "kerberosSecurityObject") ;
-	$config{'shadow'} and
-		$entry->addValue("objectclass", "shadowAccount") ;
 
-	# Create CN if blank
-	if (not $in{'cn'}) {
-		$in{'cn'} = "$in{'givenname'} $in{'surname'}" ;
+	# Create empty fields
+	if ($user->{'cn'}) {
+		$entry->{'cn'} = [$user->{'cn'}] ;
+	} else {
+		$entry->{'cn'} = ["$user->{'givenname'} $user->{'surname'}"] ;
 	}
-    &entry_from_user ($entry);;
-    $entry->{'userpassword'} = ["teste"];
+
+	if ($user->{'gecos'}) {
+		$entry{'gecos'} = [$user->{'gecos'} ;
+	} else {
+    	$entry{'gecos'} = ["$user->{'givenname'} $user->{'sn'}"] ;
+	}
+	
+	if ($user->{'mail'}) {
+		$entry{'mail'} = [$user->{'mail'}] ;	
+	} else {
+    	$entry{'mail'} = [$uid . "@" . $config{'maildomain'}] ;
+	}
+
+	# This should be more automatic, like in the useradmin module
+	if ($user->{'homedirectory'}) {
+		$entry{'homedirectory'} = [$user->{'homedirectory'}] ;
+	} else {
+    	$entry{'homedirectory'} = [$config{'homes'} . "/$uid"] ;
+	}
+    # posixAccount
+    $entry->{'uid'} = [$user->{'uid'}];
+    $entry->{'uidnumber'} = [$user->{'uidnumber'}];
+    $entry->{'gidnumber'} = [$user->{'gidnumber'}];
+    $entry->{'homedirectory'} = [$user->{'homedirectory'}];
+    $entry->{'loginshell'} = [$user->{'loginshell'}];
+    $entry->{'sn'} = [$user->{'sn'}];
+
+    if ($config{'outlook'}) {
+        $entry->{'givenname'} = [$user->{'givenname'}];
+        $entry->{'title'} = [$user->{'title'}];
+        $entry->{'organizationname'} = [$user->{'organizationname'}];
+        $entry->{'department'} = [$user->{'department'}];
+        $entry->{'physicaldeliveryofficename'} = [$user->{'physicaldeliveryofficename'}];
+        $entry->{'telephonenumber'} = [$user->{'telephonenumber'}];
+        $entry->{'mobile'} = [$user->{'mobile'}];
+        $entry->{'pager'} = [$user->{'pager'}];
+        $entry->{'officefax'} = [$user->{'officefax'}];
+        $entry->{'comment'} = [$user->{'comment'}];
+    }
+
+    if ($config{'shadow'}) {
+		$entry->addValue("objectclass", "shadowAccount") ;
+		# Need to add shadow attributes
+	}
+
+    if ($config{'kerberos'}) { 
+		$entry->addValue("objectclass", "kerberosSecurityObject") ;
+		# Need to add Kerberos attributes
+	}
+
+    $entry->{'userpassword'} = ["*"];
     $conn->add ($entry);
     if ($err = $conn->getErrorCode ()) {
-        &error ("add ($dn): $err:" . $conn->getErrorString ());
-    }
-    return $dn;
+        return [ -1, "user add ($dn): $err: " . $conn->getErrorString () ];
+    } else {
+		return [ $entry->{'uidNumber'}, $dn ] ;
+	}
 }
 
 
