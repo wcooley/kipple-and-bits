a8151c84d176a59c3725fb5bbc5032621697ae68
Wil Cooley <wcooley@nakedape.cc>
2001-12-26 21:13:47 +0000

    Move more global stuff here.  Implement eval checking for 'use' of Mozilla LDAP modules.

--- work/webmin/directory_mgr/directory-lib.pl
+++ work/webmin/directory_mgr/directory-lib.pl
@@ -9,14 +9,33 @@
 #
 
 do '../web-lib.pl' ;
-$|=1;
+$|=1 ;
+
+use strict ;
+no strict "vars" ;
 
 &init_config() ;
 
 if ("$config{'directory_type'}" eq "LDAP") {
+
+	# PerlLDAP stuff
+	eval {
+    	use Mozilla::LDAP::Conn;
+    	use Mozilla::LDAP::Utils;
+	} ;
+	# 'use' caused an error, so the modules probably aren't
+	# installed
+	if ($@) {
+    	&error($text{'ldap_modules_not_installed'},
+        	$text{'perl_eval_err'}, $@) ;
+	}
+ 
 	require "ldap_users.pl" ;
 	require "ldap_groups.pl" ;
 	require "ldap_misc.pl" ;
+
+	# Global vars
+	$conn = "" ;
 }
 
 require "users.pl" ;
