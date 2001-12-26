aebcb7ec1c0d287d8c105c5f855064777299cbf7
Wil Cooley <wcooley@nakedape.cc>
2001-12-26 21:44:18 +0000

    Hash key text cleanups.

--- work/webmin/directory_mgr/users.pl
+++ work/webmin/directory_mgr/users.pl
@@ -36,34 +36,34 @@ sub user_from_form
     my ($in) = @_;
 
     # posixAccount
-    $uid = $in{uid};
-    $uidnumber = $in{uidnumber};
-    $gidnumber = $in{gidnumber};
-    $gecos = $in{gecos};
-    $homedirectory = $in{homedirectory};
-    $loginshell = $in{loginshell};
+    $uid = $in{'uid'};
+    $uidnumber = $in{'uidnumber'};
+    $gidnumber = $in{'gidnumber'};
+    $gecos = $in{'gecos'};
+    $homedirectory = $in{'homedirectory'};
+    $loginshell = $in{'loginshell'};
 
     # address book data
-    $cn = $in{cn};
-    $sn = $in{sn};
-    $givenname = $in{givenname};
-    $title = $in{title};
-    $organizationname = $in{organizationname};
-    $department = $in{department};
-    $physicaldeliveryofficename = $in{physicaldeliveryofficename};
-    $mail = $in{mail};
-    $telephonenumber = $in{telephonenumber};
-    $mobile = $in{mobile};
-    $pager = $in{pager};
-    $officefax = $in{officefax};
-    $comment = $in{comment};
-    $userpassword = $in{userpassword};
+    $cn = $in{'cn'};
+    $sn = $in{'sn'};
+    $givenname = $in{'givenname'};
+    $title = $in{'title'};
+    $organizationname = $in{'organizationname'};
+    $department = $in{'department'};
+    $physicaldeliveryofficename = $in{'physicaldeliveryofficename'};
+    $mail = $in{'mail'};
+    $telephonenumber = $in{'telephonenumber'};
+    $mobile = $in{'mobile'};
+    $pager = $in{'pager'};
+    $officefax = $in{'officefax'};
+    $comment = $in{'comment'};
+    $userpassword = $in{'userpassword'};
 
     # generate empty fields
     $gecos = "$givenname $sn" unless $gecos;
     $cn = "$givenname $sn" unless $cn;
-    $mail = "$uid@" . $config{maildomain} unless $mail;
-    $homedirectory = $config{homes} . "/$uid" unless $homedirectory;
+    $mail = "$uid@" . $config{'maildomain'} unless $mail;
+    $homedirectory = $config{'homes'} . "/$uid" unless $homedirectory;
     $userpassword = $uid unless $userpassword;
     #$userpassword = &generate_passwd ($userpassword);
 }
@@ -97,35 +97,35 @@ sub user_from_entry
     #account
 
     #don't know
-    $cn = $user->{cn}[0];
-    $sn = $user->{sn}[0];
-    $givenname = $user->{givenname}[0];
-    $userpassword = $user->{userpassword}[0];
+    $cn = $user->{'cn'}[0];
+    $sn = $user->{'sn'}[0];
+    $givenname = $user->{'givenname'}[0];
+    $userpassword = $user->{'userpassword'}[0];
     #$krbname
 
     #OutlookExpress
     #$co
-    $comment = $user->{comment}[0];
-    $department = $user->{department}[0];
+    $comment = $user->{'comment'}[0];
+    $department = $user->{'department'}[0];
     #$homephone
     #$homepostaladdress
     #$initials
     #$ipphone
     #$l
-    $mail = $user->{mail}[0];
+    $mail = $user->{'mail'}[0];
     #$manager
-    $mobile = $user->{mobile}[0];
-    $officefax = $user->{officefax}[0];
-    $organizationname = $user->{organizationname}[0];
+    $mobile = $user->{'mobile'}[0];
+    $officefax = $user->{'officefax'}[0];
+    $organizationname = $user->{'organizationname'}[0];
     #$otherfacsimiletelephonenumber
-    $pager = $user->{pager}[0];
-    $physicaldeliveryofficename = $user->{physicaldeliveryofficename}[0];
+    $pager = $user->{'pager'}[0];
+    $physicaldeliveryofficename = $user->{'physicaldeliveryofficename'}[0];
     #$postaladdress
     #$postalcode
     #$reports
     #$st
-    $telephonenumber = $user->{telephonenumber}[0];
-    $title = $user->{title}[0];
+    $telephonenumber = $user->{'telephonenumber'}[0];
+    $title = $user->{'title'}[0];
     #$url
 }
 
@@ -134,8 +134,8 @@ sub user_defaults
 {
     $uidnumber = &max_uidnumber() + 1;
     # should get these defaults fron %config or from a template
-    $gidnumber = $config{gid};
-    $loginshell = $config{shell};
+    $gidnumber = $config{'gid'};
+    $loginshell = $config{'shell'};
 }
 
 
@@ -144,31 +144,31 @@ sub entry_from_user
     my ($entry) = @_;
 
     # posixAccount
-    $entry->{uid} = [$uid];
-    $entry->{uidnumber} = [$uidnumber];
-    $entry->{gidnumber} = [$gidnumber];
-    $entry->{gecos} = [$gecos];
-    $entry->{homedirectory} = [$homedirectory];
-    $entry->{loginshell} = [$loginshell];
+    $entry->{'uid'} = [$uid];
+    $entry->{'uidnumber'} = [$uidnumber];
+    $entry->{'gidnumber'} = [$gidnumber];
+    $entry->{'gecos'} = [$gecos];
+    $entry->{'homedirectory'} = [$homedirectory];
+    $entry->{'loginshell'} = [$loginshell];
 
     # address book data
     # should identify the correct objectClass for each attribute and configure this way
 
-    $entry->{sn} = [$sn];
-    $entry->{cn} = [$cn];
-    $entry->{mail} = [$mail];
+    $entry->{'sn'} = [$sn];
+    $entry->{'cn'} = [$cn];
+    $entry->{'mail'} = [$mail];
 
-    if ($config{outlook} eq "1") {
-        $entry->{givenname} = [$givenname];
-        $entry->{title} = [$title];
-        $entry->{organizationname} = [$organizationname];
-        $entry->{department} = [$department];
-        $entry->{physicaldeliveryofficename} = [$physicaldeliveryofficename];
-        $entry->{telephonenumber} = [$telephonenumber];
-        $entry->{mobile} = [$mobile];
-        $entry->{pager} = [$pager];
-        $entry->{officefax} = [$officefax];
-        $entry->{comment} = [$comment];
+    if ($config{'outlook'} eq "1") {
+        $entry->{'givenname'} = [$givenname];
+        $entry->{'title'} = [$title];
+        $entry->{'organizationname'} = [$organizationname];
+        $entry->{'department'} = [$department];
+        $entry->{'physicaldeliveryofficename'} = [$physicaldeliveryofficename];
+        $entry->{'telephonenumber'} = [$telephonenumber];
+        $entry->{'mobile'} = [$mobile];
+        $entry->{'pager'} = [$pager];
+        $entry->{'officefax'} = [$officefax];
+        $entry->{'comment'} = [$comment];
     }
 
 	if ($config{'shadow'}) {
