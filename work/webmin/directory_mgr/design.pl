d4f488ef0e25846e8214b6f25f42cc929645ed2b
Wil Cooley <wcooley@nakedape.cc>
2002-03-04 01:14:41 +0000

    Added group object documentation.

--- work/webmin/directory_mgr/design.pl
+++ work/webmin/directory_mgr/design.pl
@@ -12,7 +12,7 @@ to transition gracefully between them: CGI input, directory objects
 
 =over 4
 
-=item LDAP entries
+=item LDAP Objects
 
 Direcotry_Mgr uses the following LDAP objectClasses and associated
 attributes for user data:
@@ -69,6 +69,11 @@ attributes for user data:
 
 =back # end inetOrgPerson
 
+=item * top
+
+The I<top> object class is required by RFC 2256, section
+5.1.
+
 =back # end LDAP Entries
 
 
@@ -121,6 +126,56 @@ The "in" hash is the representation of user data as it is submitted
 via HTTP POST.  The "in" hash will be mostly a mirror of the
 "user" hash, except where there are naming conflicts or there are
 "automatic" settings (for example, a blank userID will be generated).
+
+=back
+
+=head2 Group Objects
+
+Group objects, like user objects, are represented by the
+LDAP objectClasses and associated attributes, a "group"
+hash, and the CGI input hash.
+
+=over 4
+
+=item LDAP Objects
+
+=over 4
+
+=item * posixGroup
+
+=over 4
+
+=item * cn I<(required)>
+
+=item * gidNumber I<(required)>
+
+=item * memberUid
+
+=item * description
+
+=back
+
+=back
+
+=item The "group" Hash
+
+=over 4
+
+=item * groupName
+
+=item * groupID
+
+=item * groupDescription
+
+=item * memberUsername I<(an array of usernames)>
+
+=back
+
+=item The "in" Hash
+
+The "in" hash for group components uses the same keys as the group
+hash, except special handling is required for automatic group ID
+creation, such as for user-private-groups.
 
 =back
 
