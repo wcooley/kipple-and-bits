83b2fe35751325b0ebe67cc2e54cf08fef123c73
Wil Cooley <wcooley@nakedape.cc>
2005-03-07 05:20:32 +0000

    Added

---
+++ work/fakesync/fakesync.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2005 Naked Ape Consulting, Ltd.
+ *
+ */
+
+void sync(void) {
+    return;
+}
+
+int fsync(int fd) {
+    return 0;
+}
+
+int fdatasync(int fd) {
+    return 0;
+}
