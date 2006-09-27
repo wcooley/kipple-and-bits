45cde675c569eb37c0868739f89b6737bdc43eed
Wil Cooley <wcooley@nakedape.cc>
2006-09-27 20:56:56 +0000

    Use AppConfig; simplify processing considerably

--- small-projects/mail-utils/expire_imap.pl
+++ small-projects/mail-utils/expire_imap.pl
@@ -8,79 +8,97 @@
 
 use strict;
 use warnings;
+use AppConfig;
 use English             qw( -no_match_var );
 use File::Basename      qw( basename );
 use Mail::IMAPClient;
 use POSIX               qw( strftime );
 
-if ( ( $#ARGV + 1 ) != 2 ) {
-    &usage;
-    exit;
+my $config = AppConfig->new(
+    {
+#        ERROR => \&usage,
+    },
+    qw(
+        imap_server|server=s 
+        imap_user|user=s 
+        imap_passwd|imap_password|passwd|password=s
+        expire_days|days|D=i
+        expire_folder|folder|F=s
+        verbose|v!
+        imap_debug|debug|d!
+    )
+);
+
+$config->file(glob '~/etc/imap_expirerc');
+$config->getopt();
+
+unless ($config->expire_days()
+    and $config->expire_folder()) {
+    print {*STDERR} "Missing something\n";
+    usage();
 }
 
-# Configure here
-my $imap_server = "SERVER";
-my $imap_user   = "USERNAME";
-my $imap_passwd = "PASSWORD";
-my $imap_debug  = undef ; # Define for debugging
-# End configure here
+my $expire_secs         = $config->expire_days() * 24 * 60 * 60;
+my $imap_expire_date    = strftime("%d-%b-%Y",localtime(time - $expire_secs));
 
-my $imap_folder         = $ARGV[1];
-my $imap_seconds_older  = $ARGV[0] * 24 * 3600;
-my $imap_expire_date    = make_imap_search_date($imap_seconds_older);
-my $msgs_removed        = 0;
-my $start_msg_count     = 0;
-my $end_msg_count       = 0;
-
-print "$imap_expire_date\n";
+my %count_of_msgs = (
+    expired             => 0,
+    before_expiration   => 0,
+    after_expiration    => 0,
+);
 
 my $imap = Mail::IMAPClient->new(
-    Server   => $imap_server,
-    User     => $imap_user,
-    Password => $imap_passwd,
+    Server   => $config->imap_server(),
+    User     => $config->imap_user(),
+    Password => $config->imap_passwd(),
 );
 
 # Record the number of messages
-$start_msg_count = $imap->message_count($imap_folder);
+$count_of_msgs{'before_expiration'} 
+    = $imap->message_count($config->expire_folder());
 
-$imap->Debug(1) if ($imap_debug);
+$imap->Debug(1) if ($config->imap_debug());
 
 # Select folder
-$imap->select($imap_folder) 
-    or die "Error selecting folder '$imap_folder': '$EVAL_ERROR'";
+$imap->select($config->expire_folder()) 
+    or die "Error selecting folder '$config->expire_folder()': '$EVAL_ERROR'";
 
 # Select expired messages
 my @msgs_expired = $imap->search("SENTBEFORE $imap_expire_date");
 
 # Remove each message and increment removed counter
 for my $msgid (@msgs_expired) {
+    print "Deleting message ID $msgid\n"
+        if ($config->verbose());
+
     $imap->delete_message($msgid)
         or warn "Error deleting message '$msgid': '$EVAL_ERROR'";
 
-    $msgs_removed += 1;
+    $count_of_msgs{'expired'} += 1;
 }
 
-# IMAP requires an EXPUNGE to actually delete message
 $imap->expunge();
 
-$end_msg_count = $imap->message_count($imap_folder);
+$count_of_msgs{'after_expiration'} 
+    = $imap->message_count($config->expire_folder());
 
-printf "%-40s %6d / %6d / %6d\n", $imap_folder, $start_msg_count,
-    $msgs_removed, $end_msg_count;
+printf "%-40s %6d / %6d / %6d\n", 
+    $config->expire_folder(), 
+    $count_of_msgs{'before_expiration'},
+    $count_of_msgs{'expired'},
+    $count_of_msgs{'after_expiration'};
 
 # Print usage
 sub usage {
     my $name = basename($PROGRAM_NAME);
+
     print <<"EOF";
-Usage: $name days folder
+Usage: $name --days <days> --folder <folder>
 Where 'days' is the age of messages to expire and 'folder' is the IMAP
 folder from which to expire messages.
+
 EOF
+
+    exit 1;
 }
 
-# Generate a date given time in seconds since the epoch that can be
-#   used with an IMAP search.  Has the form '01-Jan-2000'.
-sub make_imap_search_date {
-    my ($seconds_ago) = @_;
-    return strftime("%d-%b-%Y", localtime(time - $seconds_ago));
-}
