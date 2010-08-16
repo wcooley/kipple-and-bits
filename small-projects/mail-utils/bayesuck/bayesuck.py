7b49d663e2725bd6abb01c007de5f205484646e9
Wil Cooley <wcooley@nakedape.cc>
2010-08-16 17:34:50 +0000

    Add ham, quiet support
    * Ham support adds optional 'ham_folder' config setting
    * Quiet mode doesn't print message-processed counts
    * Making ham support optional made making spam support optional too, so the
      script could be used for one or the other. I leave it up to the user to
      ensure that it's actually doing something.

--- small-projects/mail-utils/bayesuck/bayesuck.py
+++ small-projects/mail-utils/bayesuck/bayesuck.py
@@ -17,9 +17,15 @@ imap_server     = None
 imap_user       = None
 imap_passwd     = None
 spam_folder     = None
+ham_folder      = None
 
 
 if __name__ == '__main__':
+    if sys.argv[1] == '-q':
+        quiet = True 
+    else
+        quiet = False
+
     if os.access(rcfile, os.R_OK):
         execfile(rcfile)
 
@@ -27,11 +33,17 @@ if __name__ == '__main__':
 
     conn = SimpleIMAP4(imap_server)    
     conn.login(imap_user, imap_passwd)
-    cnt = conn.folder_to_mbox(spam_folder, mbox, False)
 
-    os.system('sa-learn --spam --mbox %s' % mbox)
+    if spam_folder is not None:
+        spam_cnt = conn.folder_to_mbox(spam_folder, mbox, False)
+        os.system('sa-learn --spam --mbox %s' % mbox)
+
+    if ham_folder is not None:
+        ham_cnt = conn.folder_to_mbox(ham_folder, mbox, False)
+        os.system('sa-learn --ham --mbox %s' % mbox)
 
     os.unlink(mbox)
     conn.expunge()
 
-    print "Fed %d messages into Bayes" % cnt
+    if not quiet:
+        print "Fed %d spam and %d ham messages into Bayes" % ( spam_cnt, ham_cnt )
